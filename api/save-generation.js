f6f02fb3a40e7a0ae4740971ddcc6f9f9e0fd515
rjrizokas <152369340+rjrizokas@users.noreply.github.com>
2025-04-19 12:03:24 +0200

    Update save-generation.js

--- api/save-generation.js
+++ api/save-generation.js
@@ -1,4 +1,12 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', 'https://rjrizokas.github.io');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(204).end(); // Preflight запрос отдаем пустым
+  }
+
   if (req.method !== "POST") {
     return res.status(405).json({ error: "Only POST allowed" });
   }
